cd5241924d78928e9cf8ed71fa34d3efcf08c001
kamel nazar <kamel.husseinnadar313@gmail.com>
2022-08-31 11:28:31 +0300

    Update schema

--- api/getusers.js
+++ api/getusers.js
@@ -8,6 +8,7 @@ const prisma = new PrismaClient()
         const allusers = await prisma.student.findMany({
             select: {
                 id:true,
+                
             },
         });
         if (allusers==0){
