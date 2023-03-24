2fd43992f76df6ed5778e7e8fc3f7225f5474e74
kamel nazar <kamel.husseinnadar313@gmail.com>
2023-03-24 11:43:32 +0200

    update getdata file

--- api/getdata.js
+++ api/getdata.js
@@ -2,20 +2,20 @@ import prisma from "../lib/prisma";
 
 export default async function handler(req, res) {
     try {
-        const data = await prisma.datatb.findMany();
-        if (data.length === 0) {
-          return res.status(404).json({ message: "No data found" });
-        }
-        const jsonData = JSON.stringify(data);
-        const fileName = 'data.json';
-        fs.writeFile(fileName, jsonData, (err) => {
-          if (err) {
-            console.error(err);
-            return res.status(500).json({ message: "Error file" });
+        const data = await prisma.datatb.findMany({
+          select:{
+            id:true,
+            humidity:true,
+            temp:true,
+            timeupdate:true,
           }
-          console.log(`Data saved to ${fileName}`);
-          res.status(200).json({ message: "Data was received successfully" });
         });
+        if (data.length === 0) {
+          return res.status(404).json({ humidity: "No data found",temp: "No data found",timeupdate: "2023-03-24T15:30:00.000Z" });
+        }
+        else{
+          res.status(200).json(data);
+        }
       } catch (error) {
         res.status(400).json({ message: error.message });
       }
